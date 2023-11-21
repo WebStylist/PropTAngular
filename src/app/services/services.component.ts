87a1caa67beeead21461403e429368fec5afda2d
WebStylist <148942742+WebStylist@users.noreply.github.com>
2023-11-21 17:49:46 +0530

    Saikiran branch (#1)
    * commited changes in home.component.html file
    
    * portfolio zip file
    
    moved by don to the saikiran branch

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent {
+  expandedCard: string | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleCard(cardName: string): void {
+    this.expandedCard = this.expandedCard === cardName ? null : cardName;
   }
-
 }
